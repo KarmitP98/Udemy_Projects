f6b077fa41971d09ee0b29adc895532c02eea43b
Karmit Personal <karmit199@gmail.com>
2020-05-07 19:04:43 -0400

    Assignment Finish: Reactive Forms

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component( {
               selector: 'app-root',
               templateUrl: './app.component.html',
               styleUrls: [ './app.component.css' ]
             } )
-export class AppComponent {
+export class AppComponent implements OnInit {
+  signUpForm: FormGroup;
+  invalidName = 'Test';
+  stages = [
+    'Stable', 'Critical', 'Finished'
+  ];
+
+
+  ngOnInit(): void {
+    this.signUpForm =
+      new FormGroup( {
+                       'name': new FormControl( null,
+                                                [ Validators.required,
+                                                  this.nameValidateSync.bind( this ) ] ),
+                       'email': new FormControl( null, [ Validators.required,
+                                                         Validators.email ] ),
+                       'stage': new FormControl( null, Validators.required )
+                     } );
+
+  }
+
+  submit(): void {
+    console.log( this.signUpForm.value );
+  }
+
+  nameValidateSync( control: FormControl ): { [strKey: string]: boolean } {
+    if ( this.invalidName === control.value ) {
+      return { 'nameForbidden': true };
+    }
+    return null;
+  }
+
 }
