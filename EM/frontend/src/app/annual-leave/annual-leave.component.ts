a314b038994489de4a33830daf7dc5de6c448a2b
Karmit Personal <karmit199@gmail.com>
2020-05-14 01:25:52 -0400

    EM: Leave Works

--- EM/frontend/src/app/annual-leave/annual-leave.component.ts
+++ EM/frontend/src/app/annual-leave/annual-leave.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { LeaveService } from "../shared/leave.service";
+import { EmployeeService } from "../shared/employee.service";
+import { Subscription } from "rxjs";
+import { Leave } from "../shared/model/leaves.model";
+import { NgForm } from "@angular/forms";
 
 @Component( {
               selector: "app-annual-leave",
               templateUrl: "./annual-leave.component.html",
               styleUrls: [ "./annual-leave.component.css" ]
             } )
-export class AnnualLeaveComponent implements OnInit {
+export class AnnualLeaveComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  leaveSub: Subscription;
+  currentLeaves: Leave[] = [];
+  userId: number;
+  @ViewChild( "leaveForm", { static: false } ) leaveForm: NgForm;
+
+  constructor( private leaveService: LeaveService, private employeeService: EmployeeService ) { }
 
   ngOnInit() {
+    this.leaveService.fetchLeaves();
+    this.userId = this.employeeService.employeeSubject.getValue().userId;
+    this.leaveSub = this.leaveService.leaveSubject.subscribe( value => {
+      if ( value ) {
+        this.currentLeaves = value;
+      }
+    } );
   }
 
+  ngOnDestroy(): void {
+
+  }
+
+  onSubmit(): void {
+    this.leaveService.addLeave( this.userId, this.leaveForm.value.startDate, this.leaveForm.value.endDate, this.leaveForm.value.reason );
+    this.leaveForm.resetForm();
+    this.currentLeaves = this.leaveService.leaveSubject.getValue();
+  }
 }
--- EM/frontend/src/app/shared/leave.service.ts
+++ EM/frontend/src/app/shared/leave.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Leave } from "./model/leaves.model";
-import { Subject } from "rxjs";
+import { BehaviorSubject, Subject } from "rxjs";
 import { tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
-import { ADMIN_STATUS } from "./employee.service";
+import { ADMIN_STATUS, EmployeeService } from "./employee.service";
 
 @Injectable( {
                providedIn: "root"
@@ -11,10 +11,12 @@ import { ADMIN_STATUS } from "./employee.service";
 export class LeaveService {
 
   leavesChanged = new Subject<Leave[]>();
+  leaveSubject = new BehaviorSubject<Leave[]>( null );
   leaveServerUrl = "https://employee-managment-f5252.firebaseio.com/leaves.json";
   private leaves: Leave[] = [];
+  userId: number;
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient, private employeeService: EmployeeService ) { }
 
   getLeaves() {
     return this.leaves;
@@ -22,21 +24,39 @@ export class LeaveService {
 
   setLeaves( leaves: Leave[] ) {
     this.leaves = leaves;
-    this.leavesChanged.next( this.leaves );
+    this.leaveSubject.next( this.getCurrentLeaves() );
   }
 
   addLeave( userId: number, startDate: Date, endDate: Date, reason: string ) {
     const leaveId = this.leaves ? this.leaves.length : 0;
-    const leave = new Leave( userId, leaveId, startDate, endDate, reason, ADMIN_STATUS.pending );
+    this.leaves.push( new Leave( userId, leaveId, startDate, endDate, reason, ADMIN_STATUS.pending ) );
+    this.storeLeaves();
+    this.leaveSubject.next( this.getCurrentLeaves() );
   }
 
   fetchLeaves() {
-    this.http.get<Leave[]>( this.leaveServerUrl ).pipe( tap( leaves => {this.setLeaves( leaves );} ) ).subscribe();
+    this.http.get<Leave[]>( this.leaveServerUrl ).pipe( tap( leaves => {
+      if ( leaves ) {
+        this.setLeaves( leaves );
+      }
+    } ) ).subscribe();
+    this.userId = this.employeeService.employeeSubject.getValue().userId;
   }
 
   storeLeaves() {
-    const leaves = this.getLeaves();
-    this.http.put<Leave[]>( this.leaveServerUrl, leaves ).subscribe();
+    this.http.put<Leave[]>( this.leaveServerUrl, this.getLeaves() ).subscribe();
+  }
+
+  getCurrentLeaves() {
+    let temp: Leave[] = [];
+    if ( this.leaves ) {
+      for ( let l of this.leaves ) {
+        if ( l.userId === this.userId ) {
+          temp.push( l );
+        }
+      }
+    }
+    return temp;
   }
 
 }
