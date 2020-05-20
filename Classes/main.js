029a76a5dcd712aa103f3700b8f249a5da0c3d1b
Karmit Personal <karmit199@gmail.com>
2020-05-19 23:37:52 -0400

    Removed Angular Android and Ang1

--- Classes/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var point_1 = require("./point");
-var pointA = new point_1.Point(2);
-pointA.setY(3);
-pointA.drawPoint();
--- Classes/main.ts
+++
@@ -1,5 +0,0 @@
-import {Point} from './point'
-
-let pointA = new Point(2);
-pointA.setY(3);
-pointA.drawPoint();
